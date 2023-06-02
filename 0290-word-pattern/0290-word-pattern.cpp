1996dc4873cedb9de7979903f38c07c9cbbd9097
Pranjal Sinha <73887459+Pksinha12@users.noreply.github.com>
2023-06-03 03:36:51 +0530

    Time: 4 ms (38.62%), Space: 6.5 MB (45.30%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        unordered_map<char, string> mp1;
+        unordered_map<string, char> mp2;
+        stringstream ss(s);
+        int j = 0;
+        string word;
+        while(ss >> word)
+        {
+            words.push_back(word);
+            j++;
+        }
+        int ps = pattern.size();
+        int strs = words.size();
+        if (ps != strs) return false;
+        for (int i = 0; i < ps; i++)
+        {
+            mp1[pattern[i]] = words[i];
+        }
+        for (int i = 0; i < strs; i++)
+        {
+            mp2[words[i]] = pattern[i];
+        }
+        for (int i = 0; i < min(ps,strs); i++)
+        {
+            if (mp1[pattern[i]] != words[i] or mp2[words[i]] != pattern[i])
+                return false;
+        }
+        return true;
+    }
+};
