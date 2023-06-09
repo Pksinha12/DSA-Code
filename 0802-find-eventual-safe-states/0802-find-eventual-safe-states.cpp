295083d5502babd0179d07feeb2dcaf4fe25d247
Pranjal Sinha <73887459+Pksinha12@users.noreply.github.com>
2023-06-10 01:23:26 +0530

    Time: 244 ms (15.35%), Space: 62.1 MB (10.62%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -31,18 +31,49 @@ private:
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
     {
-        V = graph.size();
-        vis.resize(V, 0);
-        pathVis.resize(V, 0);
-        
+       int V = graph.size();
+        vector<vector<int>> revGraph(V);
+        vector<int> vis(V, 0);
+        vector<int> indegree(V,0);
         for (int i = 0; i < V; i++)
         {
-            if (!vis[i]) dfsCheck(i, graph);
+            for (auto it : graph[i])
+            {
+                revGraph[it].push_back(i);
+            }
+        }
+        queue<int> q;
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : revGraph[i])
+                indegree[it]++;
         }
         for (int i = 0; i < V; i++)
         {
-            if (pathVis[i] == 0) safeNodes.push_back(i);
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+                vis[i] = 1;
+            }
         }
+        vector<int> safeNodes;
+        while (!q.empty())
+        {
+            int node = q.front();
+            safeNodes.push_back(node);
+            q.pop();
+            
+            for (auto neighbour : revGraph[node])
+            {
+                indegree[neighbour]--;
+                if (!vis[neighbour] and indegree[neighbour] == 0)
+                {
+                    q.push(neighbour);
+                    vis[neighbour] = 1;
+                }
+            }
+        }
+        sort(safeNodes.begin(), safeNodes.end());
         return safeNodes;
     }
 };
