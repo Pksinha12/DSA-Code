120a79d10233752f937ed6d7d9485fab2840d14e
Pranjal Sinha <73887459+Pksinha12@users.noreply.github.com>
2023-06-10 07:35:39 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int V, int E, int src){
+        // code here
+        vector<vector<int>> adj(V);
+        for (int i = 0; i < E; i++)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        vector<int> dis(V,INT_MAX);
+        vector<int> vis(V, 0);
+        queue<int> q;
+        q.push(src);
+        vis[src] = 1;
+        dis[src] = 0;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            for (auto neighbour : adj[node])
+            {
+                if (!vis[neighbour])
+                {
+                    vis[neighbour] = 1;   
+                    dis[neighbour] = min(dis[neighbour], dis[node]+1);
+                    q.push(neighbour);
+                }
+            }
+        }
+        for (int i = 0; i < V; i++)
+        {
+            if (dis[i] == INT_MAX)
+            dis[i] = -1;
+        }
+        return dis;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m; cin >> n >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for(int j=0; j<2; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src; cin >> src;
+
+        Solution obj;
+
+        vector<int> res = obj.shortestPath(edges, n, m, src);
+
+        for (auto x : res){
+            cout<<x<<" ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
