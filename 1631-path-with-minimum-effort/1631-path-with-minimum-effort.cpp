d7d3d30add3a32567afb4db30368fccfb60a8cd6
Pranjal Sinha <73887459+Pksinha12@users.noreply.github.com>
2023-06-13 06:08:25 +0530

    Time: 208 ms (36.77%), Space: 36.4 MB (22.32%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n = heights.size();
+        int m = heights[0].size();
+
+        set <pair<int, pair<int,int>>> pq;
+        vector<vector<int>> minDiff(n, vector<int>(m, 1e9));
+        
+        pq.insert({0, {0, 0}});
+        minDiff[0][0] = 0;
+        
+        vector<int> delcol = {-1, 0, 1, 0};
+        vector<int> delrow = {0, 1, 0, -1};
+        while (!pq.empty())
+        {
+            auto it = *pq.begin();
+            pq.erase(it);
+            int diff = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            
+            if (row == n-1 and col == m-1) return diff;
+            for (int k = 0; k < 4; k++)
+            {
+                int nrow = row + delrow[k];
+                int ncol = col + delcol[k];
+                
+                if (nrow >= 0 and ncol >= 0 and nrow < n and ncol < m)
+                {
+                    int maxEffort = max(abs(heights[nrow][ncol] - heights[row][col]), diff);
+                    if (maxEffort < minDiff[nrow][ncol])
+                    {
+                        minDiff[nrow][ncol] = maxEffort;
+                        pq.insert({minDiff[nrow][ncol], {nrow, ncol}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
