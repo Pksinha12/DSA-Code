49b7742730b55701f695b432bb81adbb977e5372
Pranjal Sinha <73887459+Pksinha12@users.noreply.github.com>
2023-06-17 05:53:49 +0530

    Time: 8 ms (90.14%), Space: 20.9 MB (72.88%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -17,9 +17,10 @@ private:
         
         
         int lh = helper(root->left);
+        if (lh == -1) return -1;
         int rh = helper(root->right);
+        if (rh == -1) return -1;
         
-        if (lh == -1 or rh == -1) return -1;
         if (abs(lh - rh) > 1) return -1;
         
         return 1 + max(lh, rh);
