89bf60e423ce017cdada90ff233678307d9c4b0d
Pranjal Sinha <73887459+Pksinha12@users.noreply.github.com>
2023-07-08 11:21:13 +0530

    Time: 37 ms (16.88%), Space: 22.2 MB (78.30%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
        int p1 = 0,p2 = nums.size()-1;
         while(p1 < p2){
             int mid = (p1 + p2) / 2;
-            if(mid % 2 == 1){
+            if(mid & 1){
                 mid--;
             }
             if(nums[mid] == nums[mid + 1]){
