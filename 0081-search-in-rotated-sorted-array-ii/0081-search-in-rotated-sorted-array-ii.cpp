cecc51f536cc5988ce8d556804f537f683e6c5d2
Pranjal Sinha <73887459+Pksinha12@users.noreply.github.com>
2023-08-15 15:18:14 +0530

    Time: 7 ms (31.49%), Space: 14 MB (64.09%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) 
+    {
+            if (find(nums.begin(), nums.end(), target) == nums.end()) return false;
+        return true;
+    }
+};
